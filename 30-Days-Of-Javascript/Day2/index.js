68f79460a43da03f5536f48c545619f8f16939ad
MS_Udee <115361713+devudee@users.noreply.github.com>
2023-02-25 01:59:33 +0100

    Add files via upload

--- 30-Days-Of-Javascript/Day2/index.js
+++ 30-Days-Of-Javascript/Day2/index.js
@@ -39,20 +39,98 @@ console.log(string2.repeat(2));
 // DAY 2 : EXERCISES
 // EXERCISE: LEVEL 2
 // BEGIN HERE
-
+// Using console.log() print out the following statement:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
 let quote =
   "The quote 'There is no better exercise for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
 console.log(quote);
+
+// Using console.log() print out the following quote by Mother Teresa:
+// "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
 let quote2 =
   "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same. \n with charity you give love, so don't give money but reach out your hand instead.\"";
 console.log(quote2);
+
+// Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
 let num = 10;
 console.log(typeof "10");
 console.log(Number(num));
+
+// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
 let num1 = 9.8;
 let num1Float = parseFloat(num1);
 console.log(num1Float);
 console.log(Math.round(num1Float));
-let py = "Python";
 
+// Check if 'on' is found in both python and jargon
+let py = "Python";
 console.log(py.includes("on"));
+
+let ja = "jargon";
+console.log(ja.includes("on"));
+
+// I hope this course is not full of jargon. Check if jargon is in the sentence.
+let sentence1 = "I hope this course is not full of jargon.";
+console.log(sentence1.includes("jargon"));
+
+// Generate a random number between 0 and 100 inclusively.
+let randomNum = Math.round(Math.random() * 101);
+console.log(randomNum);
+
+// Generate a random number between 50 and 100 inclusively.
+let randomNum1 = Math.floor(Math.random() * 50) + 51;
+console.log(randomNum1);
+
+// Generate a random number between 0 and 255 inclusively.
+let randomNum2 = Math.floor(Math.random() * 256);
+console.log(randomNum2);
+
+// Access the 'JavaScript' string characters using a random number.
+let string3 = "Javascript";
+let numIndex = Math.floor(Math.random() * string3.length);
+let numChar = string3.charAt(numIndex);
+console.log(numChar);
+
+// Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+console.log(
+  " 1\t1 \t1  \t1  \t1 \n 2 \t1 \t2 \t4 \t8 \n 3 \t1 \t3 \t9 \t27 \n 4 \t1 \t4 \t16 \t64 \n 5 \t1 \t5 \t25 \t125 "
+);
+
+// Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let sentence2 =
+  "you cannot end a sentence with because because because is a conjuction";
+console.log(sentence2.substr(30, 24));
+
+// Exercises: Level 3
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+let sentence3 =
+  "Love is the best thing in this world. Some found their love and some are still looking for their love.";
+let count = sentence3.match(/love/gi);
+console.log(count);
+let result = count.length;
+console.log(result);
+
+// Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let sentence4 =
+  "You cannot end a sentence with because because because is a conjunction";
+let count2 = sentence4.match(/because/gi);
+console.log(count2);
+let result1 = count2.length;
+console.log(result1);
+
+// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+//     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+console.log(sentence.replaceAll("%", ""));
+console.log(sentence.replace(/[$@&#%;]/g, ""));
+
+// Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let text =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+console.log(text.match(/\d+/g));
---
+++ 30-Days-Of-Javascript/Day4/index.js
@@ -0,0 +1,187 @@
+// Exercises: Level 1
+//1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+// Enter your age: 30
+// You are old enough to drive.
+
+// Enter your age:15
+// You are left with 3 years to drive.
+let age = 5 
+if(age >= 30){
+  console.log('You are old enough to drive')
+}else if(age< 18){
+  console.log('You are left with' )
+}
+else{
+  console.log('me');
+}
+//2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+// Enter your age: 30
+// You are 5 years older than me.
+
+//3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+// using if else
+// ternary operator.
+//   let a = 4
+//   let b = 3
+//   4 is greater than 3
+let a = 4;
+let b = 3;
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("a is less than b");
+}
+a > b ? console.log("a is greater than b") : console.log("a is less then b");
+
+//4. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+
+// Enter a number: 2
+// 2 is an even number
+
+// Enter a number: 9
+// 9 is is an odd number.
+
+
+let number3 = 4
+if(number3 % 2==0){
+  console.log('it\' an even number');
+}
+else(number3 % 2== 1){
+  console.log('it is an odd number');
+}
+
+// Exercises: Level 2
+
+//1. Write a code which can give grades to students according to theirs scores:
+
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+let number = 60;
+if (number == 100 || number >= 80) {
+  console.log("A");
+} else if (number == 79 || number >= 70) {
+  console.log("B");
+} else if (number == 69 || number >= 60) {
+  console.log("C");
+} else if (number == 59 || number >= 50) {
+  console.log("D");
+} else if (number == 0 || number <= 49) {
+  console.log("F");
+} else {
+  console.log("Enter a Vaiid score");
+}
+
+//2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+let month = "February";
+if (month == "September" || month == "October" || month == "November") {
+  console.log("The season is Autumn");
+} else if (month == "December" || month == "January" || month == "February") {
+  console.log("The season is Winter");
+} else if (month == "March" || month == "April" || month == "May") {
+  console.log("The season is Spring");
+} else if (month == "June" || month == "July" || month == "August") {
+  console.log("The season is Summer");
+} else {
+  console.log("invalid month");
+}
+//3. Check if a day is weekend day or a working day. Your script will take day as an input.
+//     What is the day  today? Saturday
+//     Saturday is a weekend.
+
+//     What is the day today? saturDaY
+//     Saturday is a weekend.
+
+//     What is the day today? Friday
+//     Friday is a working day.
+
+//     What is the day today? FrIDAy
+//     Friday is a working day.
+let day = "SaturDay";
+
+switch (day) {
+  case "Saturday":
+  case "SaturDay":
+    console.log("Saturday is a weekend.");
+
+    break;
+  case "Friday":
+  case "FriDay":
+    console.log("Friday is a working day.");
+    break;
+
+  default:
+    console.log("invalid");
+    break;
+}
+// Exercises: Level 3
+//1. Write a program which tells the number of days in a month.
+//   Enter a month: January
+//   January has 31 days.
+
+//   Enter a month: JANUARY
+//   January has 31 day
+
+//   Enter a month: February
+//   February has 28 days.
+
+//   Enter a month: FEbruary
+//   February has 28 days.
+month = "February";
+switch (month) {
+  case "January":
+  case "JANUARY":
+    console.log("January has 31 days");
+    break;
+  case "February":
+  case "FEbruary":
+    console.log("February has 28 days");
+    break;
+  case "September":
+  case "April":
+  case "June":
+  case "November":
+    console.log("It has 30 days");
+    break;
+
+  default:
+    console.log("Enter a valid month");
+    break;
+}
+
+//2. Write a program which tells the number of days in a month, now consider leap year.
+month = "February";
+switch (month) {
+  case "January":
+  case "March":
+  case "May":
+  case "July":
+  case "August":
+  case "October":
+  case "December":
+    console.log("It has 31 days");
+    break;
+  case "February":
+    console.log("It has 29 days");
+    break;
+  case "September":
+  case "April":
+  case "June":
+  case "November":
+    console.log("It has 30 days");
+    break;
+
+  default:
+    console.log("Enter a valid month");
+    break;
+}
